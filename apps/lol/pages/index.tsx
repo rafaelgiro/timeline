c5cd9c1abde3e1efe80e0cf7b8b6e5f19013e846
Rafael Giro <me@rafaelgiro.dev>
2022-10-18 18:01:30 -0300

    feat: fonts and meta tags

--- apps/lol/pages/index.tsx
+++ apps/lol/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import { CardsWrapper } from "../modules/Cards/CardsWrapper";
 import { Sidebar } from "../modules/Sidebar";
 import { Background, CalendarWrapper } from "../modules/Timeline";
@@ -10,6 +12,21 @@ export default function Web() {
   return (
     <ThemeProvider>
       <Wrapper>
+        <Head>
+          <title>5Pots</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="Calendário com as novidades de League of Legends"
+          />
+          <meta name="theme-color" content="#141529" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta property="og:image" content="/static/thumb.jpg" />
+        </Head>
         <Sidebar />
         <Background />
         <CalendarWrapper data={data} />
