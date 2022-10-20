bb4391a93b2c11a3c870e36aee72a6dcd227bfc6
Rafael Giro <me@rafaelgiro.dev>
2022-10-20 19:15:26 -0300

    feat: simple event title following scroll

--- packages/timeline/modules/Timeline/components/CalendarSimpleEvent/styles.ts
+++ packages/timeline/modules/Timeline/components/CalendarSimpleEvent/styles.ts
@@ -1,9 +1,16 @@
 import styled from "@emotion/styled";
 
 export const SimpleEventTitle = styled.div`
-  position: absolute;
-  top: 0;
-  left: 1rem;
-  background-color: ${({ theme }) => theme.color.backgroundSecondary};
-  padding: 0.25rem 0.5rem;
+  position: relative;
+  width: 100%;
+  margin-left: 0.5rem;
+  margin-top: -1.25rem;
+
+  strong {
+    background-color: ${({ theme }) => theme.color.backgroundSecondary};
+    width: 100%;
+    position: sticky;
+    left: 0;
+    padding: 0.5rem 1rem;
+  }
 `;
