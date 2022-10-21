519078448acefef644fce594d3d9f621e25a9b99
Rafael Giro <me@rafaelgiro.dev>
2022-10-21 17:48:19 -0300

    feat: remove sejuani vitoriosa das skins empyrean

--- apps/lol/data/empyrean.ts
+++ apps/lol/data/empyrean.ts
@@ -1,15 +1,15 @@
 export const empyrean: TimelineEvent = {
   backgroundCategory: "pbe",
   borderCategory: "skins",
-  description: "Skins Empíreos + Sejuani Vitoriosa",
+  description: "Skins Empíreos",
   startDate: new Date("2022-10-18T00:00:00.000"),
   endDate: new Date("2022-10-31T00:00:00.000"),
   url: "https://www.youtube.com/watch?v=7Hv7FuK9VKM",
   image: {
     url: "https://assets.5pots.com/file/cincopots/timeline/empyrean.jpg",
-    alt: "Novas skins Empíreos e Sejuni Vitoriosa",
+    alt: "Novas skins Empíreos",
   },
-  name: "Skins Empíreos e Sejuani Vitoriosa",
+  name: "Skins Empíreos",
   champions: [
     { championId: "Ksante", name: "K'Sante" },
     { championId: "Pyke", name: "Pyke" },
@@ -18,7 +18,6 @@ export const empyrean: TimelineEvent = {
     { championId: "Lux", name: "Lux" },
     { championId: "Jhin", name: "Jhin" },
     { championId: "Vex", name: "Vex" },
-    { championId: "Sejuani", name: "Sejuani" },
   ],
   isFeatured: true,
 };
