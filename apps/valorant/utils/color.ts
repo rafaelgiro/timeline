971c1063f23092902d08eaeed6c4266c04f29ad8
Rafael Giro <me@rafaelgiro.dev>
2022-10-25 17:58:14 -0300

    feat: adjust valorant colors

--- apps/valorant/utils/color.ts
+++ apps/valorant/utils/color.ts
@@ -1,13 +1,13 @@
 export const color: ThemeColors = {
-  backgroundPrimary: "#111111",
-  backgroundSecondary: "#101924",
+  backgroundPrimary: "#080815",
+  backgroundSecondary: "#1A1A28",
   fontPrimary: "#ebe8e1",
   border: "rgba(242, 233, 237, 0.15)",
   highlight: "#fe4655",
   skins: "#78f0db",
   changes: "#0efff9",
-  champions: "#04A5BD",
+  champions: "#ebe8e1",
   pbe: "#FA645E",
   release: "#ffd1b6",
-  rumor: "#ebe8e1",
+  rumor: "#242333",
 };
